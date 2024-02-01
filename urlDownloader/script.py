9301d60b6dabee69383b4573ec0ae86697161e33
Leo <leocasti2@gmail.com>
2024-02-01 10:49:21 -0500

    fix: corrección temporal que los hilos que reciben menos bytes de lo esperado

--- urlDownloader/script.py
+++ urlDownloader/script.py
@@ -4,6 +4,8 @@ import threading
 from .utils import HEADERS, calcular_chunk_size, combine_files
 from pathlib import Path
 from urllib.parse import urlparse
+from urllib.error import ContentTooShortError
+from requests.exceptions import ChunkedEncodingError
 
 
 class urlDownloader:
@@ -25,9 +27,30 @@ class urlDownloader:
 
     def download(self):
         manager = ThreadManager(**self.__dict__)
-        results, folder_temp = manager.multi_threaded_download()
+        results, folder_temp = manager.multi_threaded_download(block=True)
         combine_files(results, self.dest)
         folder_temp.rmdir()
+
+    def start(self):
+        manager = ThreadManager(**self.__dict__)
+        manager.multi_threaded_download(block=False)
+
+
+def download(url, fin, inicio, count, path, block_sz, headers, archivo, index):
+    with requests.get(url, headers=headers, stream=True) as response:
+        response.raise_for_status()
+        while True:
+            for chunk in response.iter_content(chunk_size=block_sz):
+                if chunk:
+                    count += len(chunk)
+                    archivo.write(chunk)
+            break
+        if not count == fin - inicio + 1:
+            path.unlink()
+            raise ContentTooShortError(
+                f" Hilo {index} : El servidor envio menos bytes de los requeridos"
+            )
+        return response
 
 
 class ThreadManager(urlDownloader):
@@ -43,39 +66,50 @@ class ThreadManager(urlDownloader):
         headers = {"Range": f"bytes={inicio}-{fin}"}
         count = 0
         block_sz = 16384
-        with open(path, "wb") as archivo:
-            response = requests.get(url, headers=headers, stream=True)
-            while True:
-                for chunk in response.iter_content(chunk_size=block_sz):
-                    if chunk:
-                        count += len(chunk)
-                        archivo.write(chunk)
+
+        count_ChunkedEncodingError = 0
+        while True:
+            try:
+                with open(path, "wb") as archivo:
+                    response = download(
+                        url, fin, inicio, count, path, block_sz, headers, archivo, index
+                    )
                 break
+            except ChunkedEncodingError as e:
+                print(e)
+                if count_ChunkedEncodingError < 2:
+                    count_ChunkedEncodingError += 1
+                    continue
+                else:
+                    print("count_ChunkedEncodingError se ha generado dos veces")
+                    exit()
         return resultados.put((index, response.status_code, path))
 
-    def multi_threaded_download(self) -> tuple[Queue[tuple[int, int, Path]], Path]:
+    def multi_threaded_download(
+        self, block=True
+    ) -> tuple[Queue[tuple[int, int, Path]], Path]:
         filesize = self._get_filesize(self.url)
         chunks = calcular_chunk_size(
             filesize=filesize,
             threads_count=self.threads_count,
             minChunkFile=self.minChunkFile,
         )
-        resultados = Queue()
-        threads = []
-        folder = self.dest.parent / "temp"
-        folder.mkdir(exist_ok=True)
+        self._resultados = Queue()
+        self._threads = []
+        self._folder = self.dest.parent / "temp"
+        self._folder.mkdir(exist_ok=True)
         for index in range(0, self.threads_count):
             name = self.dest.with_name(f"{self.dest.stem}-{index}")
             filename = name.with_suffix(self.dest.suffix)
-            path = folder / filename
+            path = self._folder / filename
             thread = threading.Thread(
                 target=self._download_chunk,
-                args=(self.url, chunks[index], resultados, index, path),
+                args=(self.url, chunks[index], self._resultados, index, path),
             )
-            threads.append(thread)
+            self._threads.append(thread)
             thread.start()
+        if block:
+            for thread in self._threads:
+                thread.join()
 
-        for thread in threads:
-            thread.join()
-
-        return resultados, folder
+        return self._resultados, self._folder
